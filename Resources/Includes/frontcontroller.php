798cbdbdc4d89f22e6941194a51bc151c4609911
Jordan P <jpiepkow@gmail.com>
2015-03-20 20:41:05 -0400

    1

--- Resources/Includes/frontcontroller.php
+++ Resources/Includes/frontcontroller.php
@@ -1,5 +1,5 @@
 <?php
-// This function checks passwords to make sure they meet the requirements of 1 upper case letter 1 lower case letter 1 number and at least 7 char
+// This function checks passwords to make sure they meet the requirements of 1 upper case letter 1 lower case letter 1 number and at least 7 char.
 function valid_pass($pass)
 {
     if (!preg_match_all('$\S*(?=\S{7,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$', $pass))
