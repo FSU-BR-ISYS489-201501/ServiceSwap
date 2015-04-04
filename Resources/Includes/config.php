935805eb29df068bc773a733d08a96f3b92ee4a1
bcaughell <caugheb@ferris.edu>
2015-04-04 16:31:33 -0400

    Started VerifyEmailMessage, changed config
    Started verification page, uploaded new config file

--- Resources/Includes/config.php
+++ Resources/Includes/config.php
@@ -1,16 +1,22 @@
 <?php
-
-$servername = "mysql.isys489.com";
+// Connection credentials
+$servername = "localhost";
 $username = "isys489c_spscam";
 $password = "k5;Tpd#4(c_;";
 $dbname = "isys489c_BR_ServiceSwap";
+
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    echo ("The Connection died here");
+    echo ('                                   ');
+    echo ("Connection failed: " . $conn->connect_error);
 }
-//echo "Connected successfully";
+else {
+  echo "Connected successfully";
+}
 ?>
+
---
+++ Resources/Php/VerifyEmailMessage.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+	p.center {
+		text-align: center;
+	}
+	.button {
+		font: bold 11px Arial;
+		text-decoration: none;
+		background-color: #EEEEEE;
+		color: #333333;
+		padding: 2px 6px 2px 6px;
+		border-top: 1px solid #CCCCCC;
+		border-right: 1px solid #333333;
+		border-bottom: 1px solid #333333;
+		border-left: 1px solid #CCCCCC;
+	}
+	a.italic{
+		font-style: italic;
+	}
+</style>
+</head>
+<body>
+<p class="center">
+Thank you so much for registering!<br>
+Please check the email account you used to sign up and verify your email address by clicking on the link we sent you.<br>
+<button class="button">OK</button>
+<br>
+<br>
+<a class="italic" href="ContactUs.php">Didn't get an email?</a>
+</p>
