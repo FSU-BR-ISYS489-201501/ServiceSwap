79a3a368d4a792f0150fc5a828587a0cffa0def5
Kevin Reimer-Ranke <reimerk1@ferris.edu>
2015-04-05 22:36:26 -0400

    Updates
    Doing some more tweaking to get it working.

--- Resources/Php/ServicePurchaeNegotiation.php
+++ Resources/Php/ServicePurchaeNegotiation.php
@@ -1,39 +1,79 @@
+<!-- Kevin Ranke -->
+<!DOCTYPE HTML>
 <html>
+<title> Service Purchase Negotiation </title>
+<?php
+//DATABASE CONNECTION
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	// Connection credentials
+	$servername = "localhost";
+	$username = "isys489c_spscam";
+	$password = "k5;Tpd#4(c_;";
+	$dbname = "isys489c_BR_ServiceSwap";
+
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+		echo ("The Connection died here");
+		echo ('                                   ');
+		echo ("Connection failed: " . $conn->connect_error);
+	}
+	else {
+	echo "Connected successfully";
+	}
+}
+?>
+
 <head>
+<!-- Header here -->
 </head>
 <body>
 
+<!--  -->
 <form action="NegotiationDate" method="post">
-	<p>Date: <input type="date" name="date" size="10" maxlength="12" value="<?php if (isset($_POST['date'])) echo $_POST['date']; ?>"/>
+	<p>Date: <input type="date" name="date" value=""/>
 	<input type="submit" name="Accept" value="Accept" />
 	<button type="button" onclick="myFunction1()">Counter Offer</button>
     <script>
     function myFunction1() {
-    document.getElementById("demo1").innerHTML = '<input type="text" name="input1" size="25">';}
+    document.getElementById("date").innerHTML = '<input type="text" name="input1">';}
     </script>
-    <p id="demo1"></p>
+    <p id="date"></p>
 
 	
 <form action="NegotiationTime" method="post">
-	<p>Time: <input type="time" name="time" size="10" maxlength="12" value="<?php if (isset($_POST['time'])) echo $_POST['time']; ?>"/>
+	<p>Time: <input type="time" name="time" value=""/>
 	<input type="submit" name="Accept" value="Accept" />
 	<button type="button" onclick="myFunction2()">Counter Offer</button>
     <script>
     function myFunction2() {
-    document.getElementById("demo2").innerHTML = '<input type="text" name="input2" size="25">';}
+    document.getElementById("time").innerHTML = '<input type="text" name="input2">';}
     </script>
-    <p id="demo2"></p>
+    <p id="time"></p>
 	
 	
 <form action="NegotiationPayment" method="post">
-	<p>Payment <input type="payment" name="payment" size="10" maxlength="12" value="<?php if (isset($_POST['payment'])) echo $_POST['payment']; ?>"/>
+	Payment <input type="text" name="payment" value="" />
 	<input type="submit" name="Accept" value="Accept" />
 	<button type="button" onclick="myFunction3()">Counter Offer</button>
     <script>
     function myFunction3() {
-    document.getElementById("demo3").innerHTML = '<input type="text" name="input3" size="25">';}
+    document.getElementById("payment").innerHTML = '<input type="text" name="input3">';}
     </script>
-    <p id="demo3"></p>
+    <p id="payment"></p>
 </form>
+
+<input type="submit" name="submit" value="Submit"/>
+<a href="History.html"><button>Cancel</button></a>
+<button type="button">Reject Negotiation</button>
+
 </body>
+<footer>
+<!-- Footer goes here -->
+</footer>
 </html>
+
+
